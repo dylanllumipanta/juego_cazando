a13cb4200a3b5b85427420e64a13e0e7ca25680f
dylanllumipanta <dylanlumipanta2011@gmail.com>
2026-06-30 13:18:28 -0500

    variables y constantes agregadas

--- cazando.js
+++ cazando.js
@@ -1,11 +1,27 @@
 let canvas=document.getElementById("areajuego");
 let ctx=canvas.getContext("2d");
 
+let gatoX= canvas.height/2;
+let gatoY= canvas.width/2;
+let comidaX= 460;
+let comidaY= 10;
+
+const ALTO_GATO= 60;
+const ANCHO_GATO= 60;
+const ALTO_COMIDA= 25;
+const ANCHO_COMIDA= 25;
+
 function iniciar(){
     graficargato();
+    graficarcomida();
 }
 
 function graficargato(){
     ctx.fillStyle="gray";
-    ctx.fillRect(canvas.height/2,canvas.width/2,80,50);
+    ctx.fillRect(gatoX,gatoY,ANCHO_GATO,ALTO_GATO);
+}
+
+function graficarcomida(){
+    ctx.fillStyle="green";
+    ctx.fillRect(comidaX,comidaY,ANCHO_COMIDA,ALTO_COMIDA);
 }
